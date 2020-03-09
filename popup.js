67845f5b570715dc6aa3828657e9aa8f46601eb4
namita2310 <namitashetty01@gmail.com>
2020-03-09 08:20:33 +0530

    first commit

---
+++ popup.js
@@ -0,0 +1,13 @@
+
+
+function myFunction() {
+  console.log("haan");  
+  document.body.style.cursor = 'pointer ';
+}
+
+function nofunc(){
+  console.log("hello");
+}
+document.getElementById("mybtn").onclick=myFunction;
+
+
